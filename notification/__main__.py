c9ebb819341605b62cf93faa2b4f08e988e1436b
Sush <acharyasushmit10@gmail.com>
2024-02-18 13:19:39 -0500

    Add error detection

--- notification/__main__.py
+++ notification/__main__.py
@@ -1,11 +1,14 @@
 from pushover import Pushover, PushoverError
 
+import json
+
+
 def pushover_init():
-  #po = Pushover("a21o5exe854jysccbyi17riq25wssj")
-  po = Pushover("a21o5exe854jysccbyi17riq25wsssdfsdfasdfsdf")
+  po = Pushover("a21o5exe854jysccbyi17riq25wssj")
+  #po = Pushover("a21o5exe854jysccbyi17riq25wsssdfsdfasdfsdf")
   #test false token
-  po.user("upcv258kexr2suv1ei4d3t2kmeer9k")
-  #po.user("upcv258kexr2suv1ei4d3t2kmeer9m")
+  #po.user("upcv258kexr2suv1ei4d3t2kmeer9k")
+  po.user("upcv258kexr2suv1ei4d3t2kmeer9m")
   return po
 
 def send_message():
@@ -16,7 +19,16 @@ def send_message():
     po.send(msg)
   except PushoverError as error:
     error_s = repr(error)
-    print(error_s)
+    error_s = error_s.strip("PushoverError")
+    error_s = error_s.replace("\'", "")
+    error_s = error_s.replace("(", "")
+    error_s = error_s.replace(")", "")
+    data = json.loads(error_s)
+    if "user" in data.keys():
+      if data["user"] == "invalid":
+        print("User token invalid")
+    elif "token" in data.keys():
+      print("Application token invalid.")
 
 if __name__ == "__main__":
   po = pushover_init()
