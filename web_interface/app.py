de45882fef5386b3f7b456cec97566884d4b5912
sush2023 <acharyasushmit10@gmail.com>
2024-03-17 12:15:25 -0400

    Fix URL rendering using jinja templates

--- web_interface/app.py
+++ web_interface/app.py
@@ -1,16 +1,32 @@
 import os
 from flask import Flask, render_template, request, jsonify
+from Pushover_app import PushoverApp
 
 app = Flask(__name__)
 
+NOTIF_STATUS = False
+CALIBRATION_STATUS = False
+
+
 @app.route("/")
 def hello_world():
-    return render_template("index.html")
+    print(f"notification_object.result is: {notification_object.result}")
+    print(f"NOTIF_STATUS is: {NOTIF_STATUS}")
+    if not NOTIF_STATUS:
+        return render_template("index.html", token_status="led_red")
+    else:
+        print("detected correct value for notif_acknowledge")
+        return render_template("index.html", token_status="led_green" )
 
-@app.route("/calibration")
+@app.route("/calibration", methods=['GET', 'POST'])
 def calibration_view():
     print("calibration called")
     return render_template("calibration.html")
+
+@app.route("/notification", methods=['GET', 'POST'])
+def notification_view():
+    print("notification called")
+    return render_template("notification.html")
 
 @app.route("/process", methods=["POST"])
 def process():
@@ -21,11 +37,35 @@ def process():
         message = f"The token is: {token}"
         print(f"{message}")
         return jsonify({'message': message, 'calibration_state': 1})
-        #return render_template("calibration.html")
     except Exception as e:
         print(f"Error processing request: {e}")
         return jsonify({"error": "An error occurred"}), 500
 
+@app.route("/processToken", methods=["POST"])
+def processToken():
+    print("Got user token")
+    data = request.get_json()
+    token = data["token"]
+    notification_object.setUserToken(token)
+    print("sending message")
+    notification_object.sendMessage("TEST NOTIFICATION!")
+    return
+
+@app.route("/updateStatus", methods=["POST"])
+def updateStatus():
+    print("Got user token")
+    data = request.get_json()
+    if data["function"] == "Notification":
+        print("CHANGING NOTIF_STATUS TO TRUE")
+        global NOTIF_STATUS
+        NOTIF_STATUS = True
+    print(f"NOTIF STATUS IS: {NOTIF_STATUS}")
+    return jsonify({"status": NOTIF_STATUS}), 500
+
 if __name__ == "__main__":
     #app.run(host="0.0.0.0",debug=False)
+    app_token = "a21o5exe854jysccbyi17riq25wssj"
+    global notification_object
+    notification_object = PushoverApp(application_token= app_token)
     app.run(debug=True)
+    
--- web_interface/static/scripts.js
+++ web_interface/static/scripts.js
@@ -1,29 +1,64 @@
 // document.getElementById('inputForm').addEventListener('submit', function(event)
 function token_callback(event)
 {
-    console.log("detected click")
-    //event.preventDefault();
-    //var token = document.getElementById('token').value;
-    var token= "random";
-    let calibration_state;
-    fetch('/process', {
+  // var url = window.location.protocol + "//" + window.location.hostname;
+  // if (window.location.port) {
+  //     url += ":" + window.location.port;
+  // }
+  // url += "/notification.html";
+  // url = url.split('?')[0];
+  // setTimeout(function(){document.location.href = url},500);
+  // console.log("href is", window.location.href);
+  fetch('/notification', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    })
+  .then(window.location.href = "{{ url_for('notification') }}");
+    // 
+    // .then(response => response.json())
+    // .then(data => {
+    //   console.log("data.calibration_state: ", data.calibration_state);
+    //   window.location.href = "/notification";
+    // })
+    // .catch(error => {
+    //   console.error('Error:', error);
+    // });
+    // console.log("calibration_state is:", calibration_state);  
+};
+
+function calibration_callback()
+{
+  window.location.href = "/calibration"  
+}
+
+function token_submit_callback()
+{
+  var token = document.getElementById('user_token').value;
+  console.log("token is: ",  token)
+  fetch('/processToken', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ token: token}),
+  })
+  .then(response => response.json())
+  .then(data => {
+    document.getElementById('result').innerText = data.message;
+  })
+}
+
+function token_submit_acknowledge()
+{
+  if (document.getElementById('acknowledge').checked)
+    console.log("notif worked")
+    fetch('/updateStatus',{
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify({ token: token}),
+      body: JSON.stringify({ "function": "Notification", "boolean": "true"}),
     })
-    .then(response => response.json())
-    .then()
-    .then(data => {
-      console.log("data.calibration_state: ", data.calibration_state);
-      if (data.calibration_state)
-        window.location.href = "/calibration"
-    })
-    .catch(error => {
-      console.error('Error:', error);
-    });
-    console.log("calibration_state is:", calibration_state)
-    
-};
-  
+}
