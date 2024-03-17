3d264b51d93517d5f18534008df36c600c544151
sush2023 <acharyasushmit10@gmail.com>
2024-03-16 23:33:33 -0400

    Add preliminary web_interface implementation

--- web_interface/app.py
+++ web_interface/app.py
@@ -1,5 +1,4 @@
 import os
-
 from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
@@ -8,13 +7,25 @@ app = Flask(__name__)
 def hello_world():
     return render_template("index.html")
 
+@app.route("/calibration")
+def calibration_view():
+    print("calibration called")
+    return render_template("calibration.html")
+
 @app.route("/process", methods=["POST"])
 def process():
-    data = request.get_json()
-    name = data["name"]
-    age = data["age"]
-    message = f"Hello, {name}! You are {age} years old."
-    return jsonify({'message': message})
+    print("Inside process")
+    try:
+        data = request.get_json()
+        token = data["token"]
+        message = f"The token is: {token}"
+        print(f"{message}")
+        return jsonify({'message': message, 'calibration_state': 1})
+        #return render_template("calibration.html")
+    except Exception as e:
+        print(f"Error processing request: {e}")
+        return jsonify({"error": "An error occurred"}), 500
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    #app.run(host="0.0.0.0",debug=False)
+    app.run(debug=True)
--- web_interface/static/scripts.js
+++ web_interface/static/scripts.js
@@ -1,22 +1,29 @@
-document.getElementById('inputForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-  
-    var name = document.getElementById('name').value;
-    var age = document.getElementById('age').value;
-  
+// document.getElementById('inputForm').addEventListener('submit', function(event)
+function token_callback(event)
+{
+    console.log("detected click")
+    //event.preventDefault();
+    //var token = document.getElementById('token').value;
+    var token= "random";
+    let calibration_state;
     fetch('/process', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify({ name: name, age: age }),
+      body: JSON.stringify({ token: token}),
     })
     .then(response => response.json())
+    .then()
     .then(data => {
-      document.getElementById('result').innerText = data.message;
+      console.log("data.calibration_state: ", data.calibration_state);
+      if (data.calibration_state)
+        window.location.href = "/calibration"
     })
     .catch(error => {
       console.error('Error:', error);
     });
-  });
+    console.log("calibration_state is:", calibration_state)
+    
+};
   
